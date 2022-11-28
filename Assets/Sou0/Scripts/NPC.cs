1e0ba238e7a10d8a30acd8736858349ca8f2ae5a
ChoiSou0 <95004780+ChoiSou0@users.noreply.github.com>
2022-11-28 20:43:47 +0900

    NPC Interaction

--- Assets/Sou0/Scripts/NPC.cs
+++ Assets/Sou0/Scripts/NPC.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public enum NPCType
+{
+    None, Shop, CombinationTable
+}
+
 public class NPC : MonoBehaviour
 {
+    [SerializeField] private NPCType type;
+    [SerializeField] private GameObject NearUi;
+    [SerializeField] private bool isNearby;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +26,28 @@ public class NPC : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void InteractionNPC()
+    {
+                                                                                                                              
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-
+            Debug.Log("UI ON");
+            isNearby = true;
+            NearUi.SetActive(true);
         }
     }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("UI OFF");
+            isNearby = false;
+            NearUi.SetActive(false);
+        }
+    }
 }
--- Assets/Sou0/Scripts/Player.cs
+++ Assets/Sou0/Scripts/Player.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    SpriteRenderer sp;
-
     public float Player_Speed;
-
-
 
     // Start is called before the first frame update
     void Start()
     {
-        sp = gameObject.GetComponent<SpriteRenderer>();
+        
     }
 
     // Update is called once per frame
@@ -30,13 +26,12 @@ public class Player : MonoBehaviour
         float X = MoveX * Player_Speed * 0.01f;
         float Y = MoveY * Player_Speed * 0.01f;
 
-        if (X > 0)
-            sp.flipX = false;
-        else if (X < 0)
-            sp.flipX = true;
-
         transform.position = new Vector2(transform.position.x + X,
             transform.position.y + Y);
     }
-    
+
+    private void Interaction()
+    {
+        
+    }
 }
