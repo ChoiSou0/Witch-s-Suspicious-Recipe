b35d2ffca5e1d455748c37efed797e80dfc8b169
sketupcrobet <sketupcrobet@naver.com>
2023-04-06 10:27:27 +0900

    꾸미기, 대화, 퀘스트 수정

--- Assets/JW/Scripts/QuestManager.cs
+++ Assets/JW/Scripts/QuestManager.cs
@@ -4,33 +4,39 @@ using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
+
+using Random = UnityEngine.Random;
 
 public class QuestManager : MonoBehaviour
 {
-	public struct Quest001
+	public struct QuestOrig
 	{
 		public string QuestName;
 		public string QuestDiscription;
 		public float QuestGoal;
+		public float QuestCount;
 		public Sprite QuestImage;
 
-		public Quest001(string QuestName, string QuestDiscription, float QuestGoal, Sprite QuestImage)
+		public QuestOrig(string QuestName, string QuestDiscription, float QuestGoal, float QuestCount, Sprite QuestImage)
 		{
 			this.QuestName = QuestName;
 			this.QuestDiscription = QuestDiscription;
 			this.QuestGoal = QuestGoal;
+			this.QuestCount = QuestCount;
 			this.QuestImage = QuestImage;
 		}
 	}
 
-	public TextMeshProUGUI QuestNameText;
-	public TextMeshProUGUI QuestDiscriptionText;
-	public TextMeshProUGUI QuestCountText;
+	public GameObject[] QuestPannel = new GameObject[3];
 
-	public List<Quest001> QuestList = new List<Quest001>();
+	public List<QuestOrig> QuestList = new List<QuestOrig>();
 
-	public float QuestCount;
-	public float QuestGoalGlobal;
+	//public float QuestCount;
+	//public float QuestGoalGlobal;
+
+	public List<string> QuestItem;
+	public int[] QuestNum;
 
 	void Start()
 	{
@@ -41,7 +47,7 @@ public class QuestManager : MonoBehaviour
 	{
 		if (Input.GetKeyDown(KeyCode.Q))
 		{
-			AddQuest(Random.Range(0, 101).ToString(), Random.Range(0, 101).ToString(), Random.Range(0, 101), Resources.Load("1") as Sprite);
+			AddQuest("Quest Test", QuestItem[Random.Range(0, QuestItem.Count)], Random.Range(0, 101), Resources.Load("1") as Sprite);
 		}
 		if (Input.GetKeyDown(KeyCode.W))
 		{
@@ -49,32 +55,60 @@ public class QuestManager : MonoBehaviour
 		}
 		if (Input.GetKeyDown(KeyCode.E))
 		{
-			QuestCount++;
-			QuestCountText.text = QuestCount + " / " + QuestGoalGlobal;
-			if (QuestCount >= QuestGoalGlobal)
-			{
-				Debug.Log("Clear");
-			}
+			//QuestCount++;
+			QuestList[QuestNum[0]] = new QuestOrig(QuestList[QuestNum[0]].QuestName, QuestList[QuestNum[0]].QuestDiscription, QuestList[QuestNum[0]].QuestGoal, QuestList[QuestNum[0]].QuestCount + 1, QuestList[QuestNum[0]].QuestImage);
+			QuestList[QuestNum[1]] = new QuestOrig(QuestList[QuestNum[1]].QuestName, QuestList[QuestNum[1]].QuestDiscription, QuestList[QuestNum[1]].QuestGoal, QuestList[QuestNum[1]].QuestCount + 1, QuestList[QuestNum[1]].QuestImage);
+			QuestList[QuestNum[2]] = new QuestOrig(QuestList[QuestNum[2]].QuestName, QuestList[QuestNum[2]].QuestDiscription, QuestList[QuestNum[2]].QuestGoal, QuestList[QuestNum[2]].QuestCount + 1, QuestList[QuestNum[2]].QuestImage);
+			QuestPannel[0].gameObject.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = QuestList[QuestNum[0]].QuestCount + " / " + QuestList[QuestNum[0]].QuestGoal;
+			QuestPannel[1].gameObject.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = QuestList[QuestNum[1]].QuestCount + " / " + QuestList[QuestNum[1]].QuestGoal;
+			QuestPannel[2].gameObject.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = QuestList[QuestNum[2]].QuestCount + " / " + QuestList[QuestNum[2]].QuestGoal;
+			//if (QuestCount >= QuestGoalGlobal)
+			//{
+			//	Debug.Log("Clear");
+			//}
 		}
 	}
 
 	public void SettingQuest()
 	{
-		AddQuest("호박을 찾아서", "호박", 10, Resources.Load("1") as Sprite);
+		AddQuest("Quest Test", QuestItem[Random.Range(0, QuestItem.Count)], Random.Range(0, 101), Resources.Load("1") as Sprite);
+		AddQuest("Quest Test", QuestItem[Random.Range(0, QuestItem.Count)], Random.Range(0, 101), Resources.Load("1") as Sprite);
+		AddQuest("Quest Test", QuestItem[Random.Range(0, QuestItem.Count)], Random.Range(0, 101), Resources.Load("1") as Sprite);
+		//AddQuest("호박을 찾아서", "호박", 10, Resources.Load("1") as Sprite);
+		//AddQuest("수박을 찾아서", "수박", 10, Resources.Load("1") as Sprite);
+		//AddQuest("박을 찾아서", "박", 10, Resources.Load("1") as Sprite);
 	}
 
 	public void ResetQuest()
 	{
-		Quest001 QuestData = QuestList[Random.Range(0, QuestList.Count)];
-		QuestNameText.text = QuestData.QuestName;
-		QuestDiscriptionText.text = QuestData.QuestDiscription + "을(를) " + QuestData.QuestGoal + "개 가져오기";
-		QuestGoalGlobal = QuestData.QuestGoal;
-		QuestCountText.text = QuestCount + " / " + QuestGoalGlobal;
+		QuestNum = new int[] { -1, -1, -1 };
+		for (int i = 0; i < 3; i++)
+		{
+			int Index = Random.Range(0, QuestList.Count);
+			if (Array.IndexOf(QuestNum, Index) == -1)
+			{
+				QuestOrig QuestData = QuestList[Index];
+				QuestPannel[i].gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = QuestData.QuestName;
+				//QuestNameText.text = QuestData.QuestName;
+				QuestPannel[i].gameObject.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = QuestData.QuestDiscription + "을(를) " + QuestData.QuestGoal + "개 가져오기";
+				//QuestDiscriptionText.text = QuestData.QuestDiscription + "을(를) " + QuestData.QuestGoal + "개 가져오기";
+				//QuestGoalGlobal = QuestData.QuestGoal;
+				//QuestCount = 0;
+				QuestPannel[i].gameObject.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = QuestData.QuestCount + " / " + QuestData.QuestGoal;
+				//QuestCountText.text = QuestCount + " / " + QuestGoalGlobal;
+				QuestNum[i] = Index;
+				i++;
+			}
+			i--;
+		}
+		Debug.Log(QuestNum[0]);
+		Debug.Log(QuestNum[1]);
+		Debug.Log(QuestNum[2]);
 	}
 
 	public void AddQuest(string QuestName, string QuestDiscription, float QuestGoal, Sprite QuestImage)
 	{
-		Quest001 temp = new Quest001();
+		QuestOrig temp = new QuestOrig();
 		temp.QuestName = QuestName;
 		temp.QuestDiscription = QuestDiscription;
 		temp.QuestGoal = QuestGoal;
