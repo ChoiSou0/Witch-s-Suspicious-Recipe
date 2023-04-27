1b08fb03b207f250f6bb7007d35f633b675089f7
ChoiSou0 <95004780+ChoiSou0@users.noreply.github.com>
2023-04-27 09:39:15 +0900

    걷는 애니메이션 부분 코드 작업

--- Assets/Sou0/Scripts/Player.cs
+++ Assets/Sou0/Scripts/Player.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum Ani_Type
+{
+    IDLE, UP, DOWN, LEFT, RIGHT, RIGHTUP, 
+    RIGHTDOWN, LEFTUP, LEFTDOWN,
+}
+
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Ani_Type AniType;
     public float Speed;
     private float MaxSpeed;
-
+    
     private Rigidbody2D rb2D;
+    private Animator animator;
     private GameObject NowNPC;
 
     private void Awake()
     {
         rb2D = this.GetComponent<Rigidbody2D>();
+        animator = this.GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -26,6 +35,30 @@ public class Player : MonoBehaviour
     void Update()
     {
         Move();
+        Animation();
+    }
+
+    private void Animation()
+    {
+        switch (AniType)
+        {
+            case Ani_Type.IDLE:
+                break;
+            case Ani_Type.UP:
+                break;
+            case Ani_Type.DOWN:
+                break;
+            case Ani_Type.RIGHT:
+                break;
+            case Ani_Type.RIGHTUP:
+                break;
+            case Ani_Type.RIGHTDOWN:
+                break;
+            case Ani_Type.LEFTUP:
+                break;
+            case Ani_Type.LEFTDOWN:
+                break;
+        }
     }
 
     // 움직임 함수
@@ -39,6 +72,28 @@ public class Player : MonoBehaviour
 
         Vector3 GetVel = new Vector3(X, Y, 0);
         rb2D.velocity = GetVel;
+
+        Debug.Log(MoveX);
+        Debug.Log(MoveY);
+
+        if (MoveX == 0 && MoveY == 0)
+            AniType = Ani_Type.IDLE;
+        else if (MoveX == 0 && MoveY > 0)
+            AniType = Ani_Type.UP;
+        else if (MoveX == 0 && MoveY < 0)
+            AniType = Ani_Type.DOWN;
+        else if (MoveX > 0 && MoveY == 0)
+            AniType = Ani_Type.RIGHT;
+        else if (MoveX < 0 && MoveY == 0)
+            AniType = Ani_Type.LEFT;
+        else if (MoveX > 0 && MoveY > 0)
+            AniType = Ani_Type.RIGHTUP;
+        else if (MoveX > 0 && MoveY < 0)
+            AniType = Ani_Type.RIGHTDOWN;
+        else if (MoveX < 0 && MoveY > 0)
+            AniType = Ani_Type.LEFTUP;
+        else if (MoveX < 0 && MoveY < 0)
+            AniType = Ani_Type.LEFTDOWN;
     }
 
 }
